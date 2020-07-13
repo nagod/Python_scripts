89337966e0b57f1373380944f7ef15c2c7a06c37
deniz <schulen1994>
2020-07-13 20:55:50 +0200

    nothing to mantion

--- del_dublicates.py
+++ del_dublicates.py
@@ -1,44 +1,40 @@
-∑import os
+import os
 from PIL import Image
 from PIL import ImageChops
 from random import randrange
-"""
-def openImage():
-    return Image.open(filedialog.askopenfile(mode='r', title="Open file", filetypes=[('Images', '*.jpg')]))
-"""
-
-
-def compareImages(img1, img2):«
+from progress.spinner import Spinner
+def compareImages(img1, img2):
     diff = ImageChops.difference(img1, img2)
     if diff.getbbox():
         return False
     else:
         return True
 
-
 if __name__ == "__main__":
+
     # Declaring Varis
     files = []  # Image, Filename, Filepath
     dublicates = []
-    path = "/Users/deniz/Desktop/test/"
+    unwanted = []
+    path = input("Please enter Folderpath. /PATH/TO/YOUR/FOLDER/ \n")
     folder_name = str(randrange(1000)) + "/"
-
-    # Create Folder for Dublicates
-    # !!!for Windows uncomment next line
-    # os.chmod(path)
-    dublicate_folder = path + folder_name
-    os.mkdir(dublicate_folder)
-
-    # Iterate through al files in directory and check if its a file
+    
+    # Iterate through all files in directory and check wether its a file
     # Generate tmp Image and calc width/ height
     for file in os.listdir(path):
-        if ".DS_Store" in file:
-            os.replace(path + file, "/Users/deniz/Desktop/"+file)            
-        elif os.path.isfile(path + file):
-            tmp = Image.open(path + file)
-            width, height = tmp.size
-            files.append([tmp, file, path, width, height])
+        if os.path.isfile(path + file):
+            if ".DS_Store" in file:
+                os.replace(path + file, "/Users/deniz/Desktop/"+file)
+            elif "._" in file:
+                unwanted.append(file)
+            elif ".jpeg" or ".png" in file:
+                tmp = Image.open(path + file)
+                width, height = tmp.size
+                files.append([tmp, file, path, width, height])
 
+    # Declaring Processbar
+    spinner = Spinner('Loading ')
+    
     # Iterate trough all Images
     for counter in range(0, len(files)):
         for secondcounter in range(counter + 1, len(files)):
@@ -48,6 +44,14 @@ if __name__ == "__main__":
                 if compareImages(files[counter][0], files[secondcounter][0]):
                     if files[counter] not in dublicates:
                         dublicates.append(files[counter])
+                    spinner.next()
+        
+    # Create Folder for Dublicates
+    # !!!for Windows uncomment next line
+    # os.chmod(path)
+    dublicate_folder = path + folder_name
+    os.mkdir(dublicate_folder)
+
 
     for f in dublicates:
         current_path = f[2] + f[1]
